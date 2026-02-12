9fd937623ccba3736aa2d1a84d598c2d94529dce
Elias Gordillo <115597954+ProjectRed9080@users.noreply.github.com>
2026-02-12 16:26:28 -0500

    Delete app directory

--- app/script.test.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from 'vitest'
-import { sum } from './script.js'
-
-test('suma incorrecta', () => {
-    expect(sum(2, 2)).toBe(4);  // Este test fallará intencionalmente
-
-});
